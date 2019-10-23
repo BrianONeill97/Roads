8c61929312f9e40fa0f0fc025c58ce8297f5e9c8
unknown <C00213756@itcarlow.ie>
2019-10-23 16:50:40 +0100

    Spline Roads

---
+++ Slines Road/Assets/Scripts/ExtrudeShape.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtrudeShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector2[] verts2D = new Vector2[] 
+    {
+        new Vector2(0,1),
+        new Vector2(1,1),
+        new Vector2(1,1),
+        new Vector2(2,0),
+        new Vector2(3,0),
+        new Vector2(4,0)
+    };
+    public Vector2[] normal;
+    public float[] us;
+
+    public int[] lines = new int[]
+    {
+        0, 1,
+        2, 3,
+        3, 4,
+        4, 5
+    };
+
+}
+
+
