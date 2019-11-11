67c4155dbf66cd434c3136e710d796c997125a51
Brian O'Neill <C00213756@itcarlow.ie>
2019-11-11 21:49:24 +0000

    Updated with create on drag

--- Road Builder/Assets/Scripts/Testing/Linegeneration.cs
+++ Road Builder/Assets/Scripts/Testing/Linegeneration.cs
@@ -6,56 +6,102 @@ public class Linegeneration : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    Vector3 start = new Vector3(1, 5, 0);
-    Vector3 end = new Vector3(2, -1, 0);
+    Vector3 start = new Vector3();
+    Vector3 end = new Vector3();
+    Vector3 veryFirstPoint = new Vector3();
+
 
     public GameObject cube;
 
-    bool test = false;
+    bool firstTime = true;
 
     int i = 0;
 
+    Camera thisCamera;
+
+    List<Vector3> list;
+
+    //Test
+    Vector3 startb;
+    Vector3 distanceV;
+    Vector3 distanceVNorm;
+    float mag;
+    Quaternion rot;
+
+    //if the distance of the mouse if great enough from the start gen the blocks
 
     void Start()
     {
-        
+        thisCamera = Camera.main;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawLine(start, end, Color.white, 10000f, false);
-        GenBlocks();
+        if(firstTime == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //start = new Vector3();
+                start = Input.mousePosition;
+                start.z = (thisCamera.farClipPlane - 5) / 2;
+                start = thisCamera.ScreenToWorldPoint(start);
+                veryFirstPoint = start;
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //start = new Vector3();
+                start = end;
+                start.z = (thisCamera.farClipPlane - 5) / 2;
+                start = thisCamera.ScreenToWorldPoint(start);
+            }
+        }
+
+
+        if(Input.GetMouseButton(0))
+        {
+            end = Input.mousePosition;
+            end.z = (thisCamera.farClipPlane - 5) / 2;
+            end = thisCamera.ScreenToWorldPoint(end);
+            Debug.DrawLine(start, end, Color.white, 10000f, false);
+
+            GenBlocks();
+
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            //startb = new Vector3();
+            mag = 0;
+            distanceV = new Vector3();
+            distanceVNorm = new Vector3();
+            i = 0;
+        }
     }
+
 
 
 
     void GenBlocks()
     {
-        Vector3 startb = new Vector3();
-        Vector3 distanceV = end - start;
-        Vector3 distanceVNorm = Vector3.Normalize(distanceV);
-        float mag = Mathf.Round(Vector3.Magnitude(distanceV));
+        distanceV = end - start;
+        distanceVNorm = Vector3.Normalize(distanceV);
+        mag = Mathf.Round(Vector3.Magnitude(distanceV));
 
-        Quaternion rot = Quaternion.LookRotation(distanceV, Vector3.up);
-        
-        //for(int i = 0; i < mag; i++)
-        //{
-        //    startb = startb + (distanceVNorm * 1);
-        //    Instantiate(cube, startb, rot);
-        //}
-
+        rot = Quaternion.LookRotation(distanceV, Vector3.up);
         while(i < mag)
         {
+
             startb = startb + (distanceVNorm * 1);
-            Instantiate(cube, startb, rot);
+            startb.z = (thisCamera.farClipPlane - 5) / 2;
+            GameObject newObj = Instantiate(cube, transform);
+            newObj.transform.localPosition = startb;
+            newObj.transform.localRotation = rot;
             i++;
         }
     }
-
-    Vector3 VectorMult(Vector3 v1, Vector3 v2)
-    {
-        return new Vector3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
-    }
-
 }
